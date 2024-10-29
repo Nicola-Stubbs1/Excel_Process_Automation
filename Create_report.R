a33dca475476ad938d8a93657e577ab8685deec1
Nicola Stubbs <Nicola.Stubbs2@udal.nhs.uk>
2024-10-29 15:47:26 +0000

    Added Create report sheet and inputs dummy data.xlsx file The R script uses open xlsx to create an xlsx file, importing the dummy date then transforms the data and wirtes the results to an excel file and adds formatting

---
+++ Create_report.R
@@ -0,0 +1,48 @@
+# Import Libraries
+library(tidyverse)
+library(openxlsx)
+library(readxl)
+
+# Import data
+data_frame <- read_excel("Inputs/dummy_data.xlsx")
+
+# Summary - National
+df_national <- data_frame %>%
+  group_by (`Date Period`) %>%
+  summarise (`National Total` = sum(Metric)) %>%
+  mutate (`Organisation code` = '-',
+          `Organisation name` = 'National Total') %>%
+  select (`Organisation name`,`Organisation code`,`National Total`)
+
+# Summary - Regional
+df_regional <- data_frame %>%
+  group_by (`Region Name`,`Region Code`) %>%
+  summarise (`Regional Totals` = sum(Metric)) 
+
+# Worksheet Titles
+title <- "Metrics - National & Regional"
+sub_title <- "Metrics calculated at both national and regional levels"
+contact <- "name@email.com"
+
+# Create excel workbook
+wb <- createWorkbook()
+addWorksheet(wb, "National and Regional", gridLines = FALSE)
+setColWidths(wb, "National and Regional", cols = 1:5, widths = "auto")
+
+header <- createStyle(fontName = 'Arial', fontSize = 14, fontColour = '#FFFFFF', fgFill = '#16365C')
+title_font <-  createStyle(fontName = 'Arial' , fontSize = 16, fontColour = '#16365C')
+blue_font <-  createStyle(fontName = 'Arial' , fontSize = 12, fontColour = '#16365C')
+
+# Adding titles and styles to workbook
+writeData(wb, "National and Regional", title, startCol = 2, startRow = 2)
+addStyle(wb, "National and Regional",title_font, rows = 2, cols = 2)
+writeData(wb, "National and Regional", sub_title, startCol = 2, startRow = 3, headerStyle = blue_font)
+writeData(wb, "National and Regional", contact, startCol = 2, startRow = 4, headerStyle = blue_font)
+addStyle(wb, "National and Regional",blue_font, rows = 2, cols = c(3:4),gridExpand = TRUE)
+
+# Add data to excel workbook
+writeData(wb, "National and Regional", df_national, startCol = 2, startRow = 6, headerStyle = header, borders = 'all', borderStyle = 'thin')
+writeData(wb, "National and Regional", df_regional, startCol = 2, startRow = 8, headerStyle = header, borders = 'all', borderStyle = 'thin')
+
+# Save final excel output
+saveWorkbook(wb,"Outputs/final_output.xlsx",overwrite = TRUE)
