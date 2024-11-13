083ec749803c253281cf0a8cf0f5418c1bc1102b
STUBBS <nicola.stubbs2@nhs.net>
2024-11-13 17:47:53 +0000

    Added comments, rearranged sections and styling. Added styling to data which was previously missed. Combined styling for some rows. Added Outputs folder to save final file to.

--- Create_report.R
+++ Create_report.R
@@ -7,6 +7,7 @@ library(readxl)
 data_frame <- read_excel("Inputs/dummy_data.xlsx")
 
 # Summary - National
+# Grouping by date and creating a sum of metric
 df_national <- data_frame %>%
   group_by (`Date Period`) %>%
   summarise (`National Total` = sum(Metric)) %>%
@@ -15,11 +16,13 @@ df_national <- data_frame %>%
   select (`Organisation name`,`Organisation code`,`National Total`)
 
 # Summary - Regional
+# Grouping by region and creating a sum of metric
 df_regional <- data_frame %>%
   group_by (`Region Name`,`Region Code`) %>%
   summarise (`Regional Totals` = sum(Metric)) 
 
 # Worksheet Titles
+# Titles for the excel output
 title <- "Metrics - National & Regional"
 sub_title <- "Metrics calculated at both national and regional levels"
 contact <- "name@email.com"
@@ -27,22 +30,25 @@ contact <- "name@email.com"
 # Create excel workbook
 wb <- createWorkbook()
 addWorksheet(wb, "National and Regional", gridLines = FALSE)
-setColWidths(wb, "National and Regional", cols = 1:5, widths = "auto")
+setColWidths(wb,"National and Regional", cols = 1:4, widths = c(2, 52, 22, 20))
 
-header <- createStyle(fontName = 'Arial', fontSize = 14, fontColour = '#FFFFFF', fgFill = '#16365C')
-title_font <-  createStyle(fontName = 'Arial' , fontSize = 16, fontColour = '#16365C')
-blue_font <-  createStyle(fontName = 'Arial' , fontSize = 12, fontColour = '#16365C')
+# Set up font styles
+title_font <-  createStyle(fontName = "Arial" , fontSize = 16, fontColour = "#16365C")
+header <- createStyle(fontName = "Arial", fontSize = 14, fontColour = "#FFFFFF", fgFill = "#16365C")
+blue_font <-  createStyle(fontName = "Arial" , fontSize = 12, fontColour = "#16365C")
 
-# Adding titles and styles to workbook
+# Adding titles
 writeData(wb, "National and Regional", title, startCol = 2, startRow = 2)
-addStyle(wb, "National and Regional",title_font, rows = 2, cols = 2)
 writeData(wb, "National and Regional", sub_title, startCol = 2, startRow = 3, headerStyle = blue_font)
 writeData(wb, "National and Regional", contact, startCol = 2, startRow = 4, headerStyle = blue_font)
-addStyle(wb, "National and Regional",blue_font, rows = 2, cols = c(3:4),gridExpand = TRUE)
 
 # Add data to excel workbook
 writeData(wb, "National and Regional", df_national, startCol = 2, startRow = 6, headerStyle = header, borders = 'all', borderStyle = 'thin')
 writeData(wb, "National and Regional", df_regional, startCol = 2, startRow = 8, headerStyle = header, borders = 'all', borderStyle = 'thin')
+
+# Add styles to sheet
+addStyle(wb, "National and Regional", title_font, rows = 2, cols = 2)
+addStyle(wb, "National and Regional", blue_font, rows = c(3,4,7,9:15), cols = c(2:4), gridExpand = TRUE, stack = TRUE)
 
 # Save final excel output
 saveWorkbook(wb,"Outputs/final_output.xlsx",overwrite = TRUE)
