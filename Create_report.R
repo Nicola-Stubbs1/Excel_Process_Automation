9aae86256d0c9272ae5a4e80435af613d597ba24
Nicola-Stubbs1 <Nicola.Stubbs@locala.org.uk>
2025-08-18 12:58:56 +0100

    Added excel template with formulas, data bars and conditional formatting Updated Create report script with comments and new template addition Removed the xlsx files saved in the repo previously

--- Create_report.R
+++ Create_report.R
@@ -1,3 +1,6 @@
+
+#### set up script ####
+
 # Import Libraries
 library(tidyverse)
 library(openxlsx)
@@ -10,13 +13,16 @@ source("src/functions.r")
 import_location <- "Inputs/"
 output_location <- "Outputs/"
 
-# Get current month name
+#### Get current month   ####
 current_month <- get_month()
-current_month_filename <- paste0(current_month,"_dummy_data")
 
-# Import data
+#### Import data  ####
+# set up filename - this is the file to import
+current_month_filename <- paste0(current_month,"_dummy_data")
+# Import csv file 
 data_frame <- import_csv(import_location, current_month_filename)
 
+# Create summary- at National Level 
 # Summary - National
 # Grouping by date and creating a sum of metric
 df_national <- data_frame %>%
@@ -26,7 +32,7 @@ df_national <- data_frame %>%
           `Organisation name` = 'National Total') %>%
   select (`Organisation name`,`Organisation code`,`National Total`)
 
-# Summary - Regional
+# Create summary - Regional level
 # Grouping by region and creating a sum of metric
 df_regional <- data_frame %>%
   group_by (`Region Name`,`Region Code`) %>%
@@ -37,6 +43,8 @@ df_regional <- data_frame %>%
 title <- "Metrics - National & Regional"
 sub_title <- "Metrics calculated at both national and regional levels"
 contact <- "name@email.com"
+
+#### creates Excel workbook from scratch - including the formatting ####
 
 # Create excel workbook
 wb <- createWorkbook()
@@ -63,4 +71,15 @@ addStyle(wb, "National and Regional", blue_font, rows = c(3,4,7,9:15), cols = c(
 
 # Save final excel output
 output_file_name <- paste0(output_location, current_month,"_final_output.xlsx")
+saveWorkbook(wb,output_file_name,overwrite = TRUE)
+
+
+#### Using a pre-loaded excel template  #####
+
+wb<-loadWorkbook("Template.xlsx") # Template uses formulas, conditional formatting and data bars
+# Writes whole data set into All data sheet
+writeData(wb, "All Data", data_frame, startCol = 1, startRow = 1, colNames = TRUE)
+
+# creates a file name and saves to Outputs folder
+output_file_name <- paste0(output_location, current_month,"_final_output_Template.xlsx")
 saveWorkbook(wb,output_file_name,overwrite = TRUE)
